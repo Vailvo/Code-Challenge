a25ad2c704309689597b52244930efee660b2598
Shazé Williams <44694518+Vailvo@users.noreply.github.com>
2022-06-22 23:01:01 -0700

    Debug Cors and connect index and delete

--- server/src/controllers/trip.controller.ts
+++ server/src/controllers/trip.controller.ts
@@ -1,15 +1,19 @@
 import mongoose from "../../imports";
 
+// TODO: Abstract Cors into method or figure out global strategy
 const Trip = require("../models/Trip.model");
 // await is waiting for a promise
 // the result of the mongoose is then returned
 const showTrip = async (req: any, res: any) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3001");
+
   Trip.findOne({ _id: req.params.id })
     .then((trips: any) => res.json({ trips }))
     .catch((error: any) => res.json({ message: "NOPE", error }));
 };
 
 const tripIndex = async (req: any, res: any) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3001");
   Trip.find()
     .then((trips: any) => res.json({ trips }))
     .catch((error: any) =>
@@ -18,6 +22,8 @@ const tripIndex = async (req: any, res: any) => {
 };
 
 const createTrip = async (req: any, res: any) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3001");
+
   console.log(req.body);
   const { date, title, desc, destination, publisher } = req.body;
   Trip.create({
@@ -34,6 +40,8 @@ const createTrip = async (req: any, res: any) => {
 };
 
 const updateTrip = async (req: any, res: any) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3001");
+
   const { date, title, desc, destination, publisher } = req.body;
   Trip.findOneAndUpdate(
     { _id: req.params.id },
@@ -46,6 +54,8 @@ const updateTrip = async (req: any, res: any) => {
 };
 
 const deleteTrip = async (req: any, res: any) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3001");
+
   Trip.deleteOne({ _id: req.params.id })
     .then((trip: any) => res.json({ trip }))
     .catch((error: any) =>
